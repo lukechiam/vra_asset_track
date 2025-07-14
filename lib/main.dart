66a79f84c5e29171ab79d3ab6d5d83cf1bcf3564
Luke Chiam <lukechiam@gmail.com>
2025-07-14 22:06:50 +1000

    Removed dependency to fix error

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:vra_asset_track/activity_picker.dart';
